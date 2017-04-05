ac03995dfac5bd41dcb8b86db5cda4f9695b8939
chasms <chasms@chas.ms>
2017-04-05 15:19:43 -0400

    returned app

--- index.js
+++ index.js
@@ -2,19 +2,10 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./fetch-setup');
 
-// const {getAll} = require('./data/pets')
-// const ALL_PETS = getAll();
-// const ADOPTED_PETS = [ALL_PETS[0].id];
-
-// const App = require('./components/App');
-const Filters = require('./components/Filters');
-
-const totallyChangedType = (type) => {
-  console.log(`Changed to type ${type}`)
-}
+const App = require('./components/App');
 
 ReactDOM.render(
-  <Filters filters={{type: "micropig"}} onChangeType={totallyChangedType}/>,
+  <App />,
   document.getElementById('main')
 );
 
