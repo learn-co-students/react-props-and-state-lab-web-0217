3e92961d1939ba3137f4b7289532fe46fcb7a7f3
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-05 11:43:06 -0400

    added key to the mapped pets

--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -5,7 +5,10 @@ const Pet = require('./Pet');
 class PetBrowser extends React.Component {
   render() {
     const petsWithAdoptStatus = this.props.pets.map((petInfo) =>
-      <Pet pet={petInfo} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(petInfo.id)}/>
+      <Pet key={petInfo.id}
+        pet={petInfo}
+        onAdoptPet={this.props.onAdoptPet}
+        isAdopted={this.props.adoptedPets.includes(petInfo.id)}/>
     )
   return (
     <div className="ui cards">
